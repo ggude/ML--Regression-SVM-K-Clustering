4c0a303d9748d597729f9963d9cf952497292981
ggude <gnapika1@gmail.com>
2021-10-13 09:29:35 +0530

    Update costFunction.m

--- Machine Learning-Stanford University- Andrew N.G/machine-learning-ex2/ex2/costFunction.m
+++ Machine Learning-Stanford University- Andrew N.G/machine-learning-ex2/ex2/costFunction.m
@@ -1,5 +1,5 @@
 function [J, grad] = costFunction(theta, X, y)
-%COSTFUNCTION Compute cost and gradient for logistic regression
+%COSTFUNCTION Compute cost and gradient for the logistic regression
 %   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
 %   parameter for logistic regression and the gradient of the cost
 %   w.r.t. to the parameters.
